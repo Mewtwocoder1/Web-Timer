b8fd05ae025c64ab703717cd3268e1441d88a3e1
Mewtwocoder <130798336+Mewtwocoder1@users.noreply.github.com>
2025-01-25 22:07:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const frameHitInput = document.getElementById("frameHit");
 const mainTimerLabel = document.getElementById("mainTimer");
 const subTimerLabel = document.getElementById("subTimer");
 const startButton = document.getElementById("startButton");
+const updateButton = document.getElementById("updateButton");
 const consoleFps = 59.6555;
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)(); // Create AudioContext
